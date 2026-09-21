733ada9e65a9b74d20f54ec63940e52bae472f85
Deji-design <tempdeji@gmail.com>
2026-09-21 19:39:18 +0100

    Update on major scale lesson

--- Lessons/major_scales.py
+++ Lessons/major_scales.py
@@ -2,7 +2,29 @@ def major_scales ():
 
 # Lesson begins
     print("""Welcome
-Major scales are the basics of musical knowledge. 
+
+WHAT IS A MAJOR SCALE?
+
+It is a sequence of seven different notes arranged according to a specific pattern.
+
+Majors scales are based on the Major scale formula. The Major scale formula is based on the pattern of tones and semitones. 
+The pattern is tone-tone-semitone-tone-tone-tone-semitone.
+
+WHAT ARE TONES AND SEMITONES?
+
+A tone is a full step, while a semitone is a half step.
+A piano is a visual representation of tones and semitones. 
+A semitone is the distance between two adjacent keys on a piano.
+And a tone consists of two semitones.
+
+
+AN EXAMPLE?
+The C major scale is a good example of a major scale. The C major scale consists of the following notes: C, D, E, F, G, A, B, C.
+
+
+
+
+
 
 How many major key signatures do you think exist in Western music
 A. 12
